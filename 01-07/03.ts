12a98d42a08d25bf2afc85c260f584e9381ed2c4
Stefan Temme <temme@objectcode.de>
2025-12-03 09:08:22 +0100

    day 3 star 2

--- 01-07/03.ts
+++ 01-07/03.ts
@@ -5,19 +5,31 @@ const parse = (data: string) => {
     return banks.map(bank => bank.split('').map(Number));
 }
 
-const largestBatteryInBank = (bank: number[]) => {
-    const lArray = bank.slice(0,-1);
-    let lMaxIndex = lArray.reduce((a, b,i) => lArray[a]<b?i:a, 0);
-    const rArray = bank.slice(lMaxIndex+1);
-    let rMaxIndex = rArray.reduce((a, b,i) => rArray[a]<b?i:a, 0);
-    return lArray[lMaxIndex]*10+rArray[rMaxIndex];
+const largestBatteryInBank = (bank: number[], batterieCount = 2) => {
+    const numbers = [];
+    let leftBorder = 0;
+
+    for (let i = 0; i < batterieCount; i++) {
+        const rightBorder = bank.length - batterieCount + i + 1;
+        let maxIndex = leftBorder;
+
+        for (let j = leftBorder + 1; j < rightBorder; j++) {
+            if (bank[j] > bank[maxIndex]) {
+                maxIndex = j;
+            }
+        }
+
+        numbers.push(bank[maxIndex]);
+        leftBorder = maxIndex + 1;
+    }
+    return numbers.reduce((a, b) => a * 10 + b, 0);
 }
 const solve = (data: string) => {
     const batteriesByBank = parse(data);
 
     const sum1 = batteriesByBank.reduce((sum, bank) => sum + largestBatteryInBank(bank), 0);
-
-    console.log("Sum of largest batteries in each bank:", sum1);
+    const sum2 = batteriesByBank.reduce((sum, bank) => sum + largestBatteryInBank(bank,12), 0);
+    console.log("Sum of largest batteries in each bank:","\nStar1:", sum1,"\nStar2:", sum2);
 }
 console.log("Advent of Code - 2025 - 3")
 console.log("https://adventofcode.com/2025/day/3")
